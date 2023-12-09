6461042bc95692effa5494173c186814da38dbe7
mtkuhn <junkpilemk@protonmail.com>
2023-12-09 06:06:33 -0700

    Day 9 - cleanup

--- src/main/kotlin/mkuhn/aoc/Day09.kt
+++ src/main/kotlin/mkuhn/aoc/Day09.kt
@@ -9,24 +9,17 @@ fun main() {
 }
 
 fun day09part1(input: List<String>): Int =
-    input.sumOf { line ->
-        line.split(" ").map { it.toInt() }.extrapolateNextReading()
-    }
+    input.sumOf { line -> line.split(" ").map { it.toInt() }.extrapolateNextReading() }
 
 fun day09part2(input: List<String>): Int =
-    input.sumOf { line ->
-        line.split(" ").map { it.toInt() }.extrapolatePreviousReading()
-    }
+    input.sumOf { line -> line.split(" ").map { it.toInt() }.extrapolatePreviousReading() }
 
-fun List<Int>.extrapolateNextReading(): Int =
-    generateSequence(this) { it.differencesBetweenSteps() }
-        .takeWhile { d -> d.any { it != 0 } }
-        .sumOf { it.last() }
+fun List<Int>.extrapolateNextReading(): Int = getAllDifferenceSequences().sumOf { it.last() }
 
 fun List<Int>.extrapolatePreviousReading(): Int =
-    generateSequence(this) { it.differencesBetweenSteps() }
-        .takeWhile { d -> d.any { it != 0 } }.toList()
-        .map { it.first() }.reversed()
-        .fold(0) { acc, d -> (d-acc) }
+    getAllDifferenceSequences().map { it.first() }.reversed().fold(0) { acc, d -> (d-acc) }
+
+fun List<Int>.getAllDifferenceSequences(): List<List<Int>> =
+    generateSequence(this) { it.differencesBetweenSteps() }.takeWhile { d -> d.any { it != 0 } }.toList()
 
 fun List<Int>.differencesBetweenSteps(): List<Int> = this.windowed(2).map { it.last()-it.first() }
