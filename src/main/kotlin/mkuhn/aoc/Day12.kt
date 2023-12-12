dd4660d61914c7c80faa86c3a7629b8441834e63
mtkuhn <junkpilemk@protonmail.com>
2023-12-12 12:03:38 -0700

    Day 12 - optimization attempts

--- src/main/kotlin/mkuhn/aoc/Day12.kt
+++ src/main/kotlin/mkuhn/aoc/Day12.kt
@@ -10,45 +10,64 @@ fun main() {
 
 fun day12part1(input: List<String>): Int {
     val inputArrangements = input.map { line ->
-        ArrangementData(line.substringBefore(" "),
+        ArrangementData(line.substringBefore(" "), 0,
             line.substringAfter(" ").split(",").map { it.toInt() })
     }
-    val possibleArrangements = inputArrangements.map { a -> a.findAllPossibleArrangements() }
+    val possibleArrangements = inputArrangements.mapIndexed { i, a ->
+        val arrangementCache: MutableMap<Pair<Int, Int>, List<ArrangementData>> = mutableMapOf()
+        a.findAllPossibleArrangements(arrangementCache)
+    }
     return possibleArrangements.sumOf { it.size }
 }
 
 fun day12part2(input: List<String>): Int {
     val inputArrangements = input.map { line ->
-        ArrangementData(line.substringBefore(" "),
+        ArrangementData(line.substringBefore(" "), 0,
             line.substringAfter(" ").split(",").map { it.toInt() }).unfold()
     }
-    val possibleArrangements = inputArrangements.mapIndexed { i, a -> a.findAllPossibleArrangements().apply { println("$i | ${this.size}") } }
-
+    val possibleArrangements = inputArrangements.mapIndexed { i, a ->
+        val arrangementCache: MutableMap<Pair<Int, Int>, List<ArrangementData>> = mutableMapOf()
+        a.findAllPossibleArrangements(arrangementCache).apply { println("$i | ${this.size}") }
+    }
     return possibleArrangements.sumOf { it.size }
 }
 
-data class ArrangementData(val unknown: String, val lengths: List<Int>) {
+data class ArrangementData(val fullString: String, val startPos: Int, val lengths: List<Int>) {
 
-    fun findAllPossibleArrangements(): List<ArrangementData> {
-        return if(lengths.isEmpty()) return listOf(this)
-        else { findArrangementsForGroupOfLength(lengths.first()) }
+    fun findAllPossibleArrangements(cache: MutableMap<Pair<Int, Int>, List<ArrangementData>>): List<ArrangementData> {
+        val str = fullString.substring(startPos)
+        return if(lengths.isEmpty()) listOf(this)
+        else {
+            if(cache.containsKey(str.length to this.lengths.first())) {
+                //println("cache hit")
+                cache[str.length to this.lengths.first()]!!
+            } else {
+                val result = str.findArrangementsForGroupOfLength(lengths.first(), cache)
+                cache[str.length to this.lengths.first()] = result
+                result
+            }
+        }
     }
 
-    private fun findArrangementsForGroupOfLength(arrLen: Int): List<ArrangementData> {
-        val possiblePositions = unknown.indices.windowed(arrLen, partialWindows = false)
+
+    private fun String.findArrangementsForGroupOfLength(arrLen: Int,
+                                                 cache: MutableMap<Pair<Int, Int>, List<ArrangementData>>): List<ArrangementData> {
+
+        val possiblePositions = this.indices.windowed(arrLen, partialWindows = false)
             .filter { w ->
-                    w.canBeBounded(unknown) &&
-                    w.all { unknown[it] != '.' } &&
-                    !unknown.substring(0, w.first()).contains("#") && //we can't have extra groups before this
-                    (lengths.size > 1 || !unknown.substring(w.last()+1).contains("#")) && //no trailing groups on last match
+                    w.canBeBounded(this) &&
+                    w.all { this[it] != '.' } &&
+                    !this.substring(0, w.first()).contains("#") && //we can't have extra groups before this
+                    (lengths.size > 1 || !this.substring(w.last()+1).contains("#")) && //no trailing groups on last match
                     //the below for optimization, not correctness
-                    ((unknown.substring(w.last()).length) > (lengths.drop(1).sum())+lengths.size-1) //there's enough space for the remaining groups
+                    ((this.substring(w.last()).length) > (lengths.drop(1).sum())+lengths.size-1) //there's enough space for the remaining groups
             }
 
         return possiblePositions.flatMap { p ->
-            var newUnknown = unknown.substring(p.last()+1)
-            if(p.last() < unknown.length-1) newUnknown = newUnknown.drop(1)
-            ArrangementData(newUnknown, lengths.drop(1)).findAllPossibleArrangements()
+            var offset = p.last()+2
+            ArrangementData(fullString,
+                (startPos+offset).coerceAtMost(fullString.length-1),
+                lengths.drop(1)).findAllPossibleArrangements(cache)
         }
 
     }
@@ -58,7 +77,8 @@ data class ArrangementData(val unknown: String, val lengths: List<Int>) {
         (last() == str.length-1 || str[last()+1] in "?.")
 
     fun unfold(): ArrangementData = ArrangementData(
-        (1..5).joinToString("?") { unknown },
+        (1..5).joinToString("?") { fullString },
+        0,
         (1..5).flatMap { lengths })
 }
 
